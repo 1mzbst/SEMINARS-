b2abd324246b45300ee9bc04cbbd77baa7e08fcb
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-10 20:31:32 +0300

    Добавил программу сложение с водом

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("GB lesson");
+int num = 5;
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num + " " + num2);
+
+Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int quad = num * num;
+Console.WriteLine($"Quad of {num} is {quad}");
