25c116d6f85443e4d6722654960e7b1c3f0d744f
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-10 20:32:06 +0300

    Задача с квадратом

--- seminars/seminar1/Program.cs
+++ seminars/seminar1/Program.cs
@@ -1,9 +1,4 @@
-﻿Console.WriteLine("GB lesson");
-int num = 5;
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num + " " + num2);
-
-Console.WriteLine("Input number");
+﻿Console.WriteLine("Input number");
 int num = Convert.ToInt32(Console.ReadLine());
 int quad = num * num;
 Console.WriteLine($"Quad of {num} is {quad}");
