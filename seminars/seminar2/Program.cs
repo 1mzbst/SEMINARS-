a6de5fb98fc6a9e0eb8a01b95915f20fe0506c06
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-14 21:23:27 +0300

    Задачи в группах Ошибка

--- seminars/seminar2/Program.cs
+++ seminars/seminar2/Program.cs
@@ -34,18 +34,71 @@
 
 // 918 -> 98
 
-int DeleteDecimal(int num)
-{
-    int sot = num / 100;
-    int ed = num % 10;
-    return sot * 10 + ed;
+// int DeleteDecimal(int num)
+// {
+//     int sot = num / 100;
+//     int ed = num % 10;
+//     return sot * 10 + ed;
 
-}
+// }
 
-int RandomNumber = new Random().Next(100, 1000);
+// int RandomNumber = new Random().Next(100, 1000);
 
-Console.WriteLine(RandomNumber);
+// Console.WriteLine(RandomNumber);
 
-int result = DeleteDecimal(RandomNumber);
+// int result = DeleteDecimal(RandomNumber);
 
-Console.WriteLine(DeleteDecimal(RandomNumber));
+// Console.WriteLine(DeleteDecimal(RandomNumber));
+
+// Задача3
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+
+//Задача 4
+
+// int Devid723(int num)
+// {
+//     if (num % 7 == 0 && num % 23 == 0)
+//     {
+//         Console.WriteLine("Ок");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Нет");
+//     }
+// }
+// Console.WriteLine("input number");
+// int Number = Convert.ToInt32(Console.ReadLine());
+
+// Devid723(Number);
+
+// Задача 5
+
+// void KratDecimal(double num1, double num2)
+// {
+//     if (num1 % num2 == 0)
+//     {
+//         Console.WriteLine("Кратно");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Остаток от деления {num1} / {num2} = {num1 % num2}");
+//     }
+// }
+
+// Console.WriteLine("Imput first number");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Imput second number");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+
+// KratDecimal(num1, num2);
