e8302ac5a76b898ed8d9c28e52652a8345c5fdd6
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-14 20:33:32 +0300

    Seminar2 задача1 , задача2

---
+++ seminars/seminar2/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача1
+
+// int MaxDecimal(int num)
+// {
+//     int ed = num % 10;
+//     int dec = num / 10;
+//     if (ed > dec)
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {ed}");
+
+//         return ed;
+
+
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {dec}");
+
+//         return dec;
+//     }
+// }
+
+// int RandomNumber = new Random().Next(10, 99 + 1);
+
+// MaxDecimal(RandomNumber);
+
+//Задача 2
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+
+// 782 -> 72
+
+// 918 -> 98
+
+int DeleteDecimal(int num)
+{
+    int sot = num / 100;
+    int ed = num % 10;
+    return sot * 10 + ed;
+
+}
+
+int RandomNumber = new Random().Next(100, 1000);
+
+Console.WriteLine(RandomNumber);
+
+int result = DeleteDecimal(RandomNumber);
+
+Console.WriteLine(DeleteDecimal(RandomNumber));
