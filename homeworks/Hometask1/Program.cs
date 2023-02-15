2c768bed2ba6faf256e91ccab4d4bc113d5bb6b8
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-15 23:51:54 +0300

    Задача2

--- homeworks/Hometask1/Program.cs
+++ homeworks/Hometask1/Program.cs
@@ -17,3 +17,27 @@
 //     Console.WriteLine($"Второе число ({num2}) больше первого числа ({num1}).");
 // }
 
+//Задача2.
+// Console.WriteLine("Дай первое число!");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Дай второе число!");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Дай третье число!");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2 && num1 > num3)
+// {
+//     Console.WriteLine($"Победило первое число! ({num1})");
+// }
+// else if (num1 == num2 && num1 == num3 && num3 == num2)
+// {
+//     Console.WriteLine("Ну, что ты начинаешь...");
+// }
+// else if (num2 > num1 && num2 > num3)
+// {
+//     Console.WriteLine($"Победа за вторым числом!({num2})");
+// }
+// else
+// {
+//     Console.WriteLine($"Победитель - число под номером три!({num3})");
+// }
