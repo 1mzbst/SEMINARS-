66e31f57a9ebe31951ad512d509fdbc2a394d8c0
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-15 23:49:55 +0300

    Zadacha1

---
+++ homeworks/Hometask1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Задача1
+// onsole.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Первое число ({num1}) больше второго числа({num2}).");
+// }
+// else if (num1 == num2)
+// {
+//     Console.WriteLine($"Первое число ({num1}) равно второму числу ({num2}).");
+// }
+// else
+// {
+//     Console.WriteLine($"Второе число ({num2}) больше первого числа ({num1}).");
+// }
+
