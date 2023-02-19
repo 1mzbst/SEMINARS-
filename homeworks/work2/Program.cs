b0e4faeefbf13f49e7bf5aa86c105db6b9bd93da
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-20 00:57:44 +0300

    ломаю голову

--- homeworks/work2/Program.cs
+++ homeworks/work2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10:
+
+// Console.WriteLine("Please, enter the three-digit number."); // three-digit number - Tdn
+
+// int Tdn = Convert.ToInt32(Console.ReadLine());
+
+// int SecNum = (Tdn / 10) % 10; // SecNum - second number;
+
+// Console.WriteLine($"The Second number of {Tdn} is {SecNum}.");
+
+//Задача 13:
+
+Console.WriteLine("Hey there, engage the any number! ");
+
+int ThrdNum(int AnyNum)
+{
+    double DThrdNum = (AnyNum * 0.01) % 10;
+    if (Thrd)
+}
+
+
+// int AnyNum = Convert.ToInt32(Console.ReadLine());
+
+// double DThrdNum = (AnyNum * 0.01) % 10;
+
+// int ThrdNum = Convert.ToInt32(DThrdNum);
+
+// if (ThrdNum < 100)
+// {
+//     Console.WriteLine(ThrdNum);
+// }
+// else
+// {
+//     Console.WriteLine("The third number is empsent, sorry...");
+// }
+
+
+
