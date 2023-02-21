95ecd1f18cc6332e910eba921064b3796b3bf5d3
Dmitriy Kulakov <dk4git@outlook.com>
2023-02-21 20:48:08 +0300

    Seminar4 задача 4

---
+++ seminars/seminar4/Program.cs
@@ -0,0 +1,51 @@
+﻿// int FindSum(int a)
+// {
+//     int sum = 0;
+//     for (int current = 1; current <= a; current++)
+//     {
+//         sum = sum + current;// sum+=current;
+//     }
+//     return sum;
+
+// }
+
+// Console.WriteLine("Input A ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(FindSum(a));
+
+//___________________________________________________________________________________________________________
+
+//Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+// РЕШЕНИЕ:
+
+int[] CreatRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Input array size");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input minimal value of array element");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maximal value of array element");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+//int[] myArray = CreatRandomArray(size, minValue, maxValue);
+
+ShowArray(CreatRandomArray(size, minValue, maxValue));
