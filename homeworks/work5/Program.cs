4a4b51c8d2658f6ff14bb2d1bc888cb8ef7ae106
unknown <dk4git@outlook.com>
2023-03-02 15:43:53 +0300

    work5 task 3

--- homeworks/work5/Program.cs
+++ homeworks/work5/Program.cs
@@ -5,14 +5,109 @@
 
 // Решение
 
+// int[] CreatRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int FindEvenNumber(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) count += 1;
+//     }
+//     Console.WriteLine(count);
+//     return count;
+// }
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// // ShowArray(CreatRandomArray(size, 100, 999));
+// int[] MyArray = CreatRandomArray(size, 100, 999);
+// ShowArray(MyArray);
+// FindEvenNumber(MyArray);
+
+// _________________________________________________________________________________________________
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+// Решение
+
+// int[] CreatRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void FindSum(int[] array)
+// {
+//     // int count = 0;
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i % 2 != 0) sum += array[i];
+//     }
+//     Console.WriteLine(sum);
+// }
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input maximal value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
 
 
-int[] CreatRandomArray(int size, int minValue, int maxValue)
+// int[] MyArray = CreatRandomArray(size, minValue, maxValue);
+// ShowArray(MyArray);
+// FindSum(MyArray);
+
+// _________________________________________________________________________________________________
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+// Решение
+
+double[] CreatRandomArray(int size, double minValue, double maxValue)
 {
-    int[] array = new int[size];
+    double[] array = new double[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(minValue, maxValue + 1);
+        array[i] = new Random().NextDouble(minValue, maxValue + 1);
     }
     return array;
 }
@@ -25,25 +120,31 @@ void ShowArray(int[] array)
     Console.WriteLine();
 }
 
-int FindEvenNumber(int[] array)
+void FindSum(int[] array)
 {
-    int count = 0;
+    // int count = 0;
+    int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0) count += 1;
+        if (i % 2 != 0) sum += array[i];
     }
-    Console.WriteLine(count);
-    return count;
+    Console.WriteLine(sum);
 }
 
-Console.WriteLine("Input array size");
-int size = Convert.ToInt32(Console.ReadLine());
-// ShowArray(CreatRandomArray(size, 100, 999));
-int[] MyArray = CreatRandomArray(size, 9, 100);
-ShowArray(MyArray);
-FindEvenNumber(MyArray);
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input maximal value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
 
 
+// int[] MyArray = CreatRandomArray(size, minValue, maxValue);
+// ShowArray(MyArray);
+// FindSum(MyArray);
+
+
+// _________________________________________________________________________________________________
 
 // int arrayFindEN = FindEvenNumber(CreatRandomArray(size, 100, 999));
 // ShowArray()
@@ -53,13 +154,6 @@ FindEvenNumber(MyArray);
 // int minValue = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Input maximal value of array element");
 // int maxValue = Convert.ToInt32(Console.ReadLine());
-
-
-
-
-
-
-
 
 
 // _________________________________________________________________________________________________________________________
