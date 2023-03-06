5cd1699813a27ec34d8df9f8dc18b6046387ef83
Dmitriy Kulakov <dk4git@outlook.com>
2023-03-06 13:41:44 +0300

    work5 is done and update

--- homeworks/work5/Program.cs
+++ homeworks/work5/Program.cs
@@ -30,13 +30,12 @@
 //     {
 //         if (array[i] % 2 == 0) count += 1;
 //     }
-//     Console.WriteLine(count);
+//     Console.WriteLine($" The count of even numbers is {count}.");
 //     return count;
 // }
 
 // Console.WriteLine("Input array size");
 // int size = Convert.ToInt32(Console.ReadLine());
-// // ShowArray(CreatRandomArray(size, 100, 999));
 // int[] MyArray = CreatRandomArray(size, 100, 999);
 // ShowArray(MyArray);
 // FindEvenNumber(MyArray);
@@ -78,7 +77,7 @@
 //     {
 //         if (i % 2 != 0) sum += array[i];
 //     }
-//     Console.WriteLine(sum);
+//     Console.WriteLine($"The sum of odd elements is {sum}.");
 // }
 
 // Console.WriteLine("Input array size");
@@ -102,68 +101,52 @@
 
 // Решение.
 
-double[] CreatRandomArray(int size, double minValue, double maxValue)
-{
-    double[] array = new double[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = Math.Round(minValue + new Random().NextDouble() * (maxValue + minValue), 4);
+// double[] CreatRandomArray(int size, double minValue, double maxValue)
+// {
+//     double[] array = new double[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = Math.Round(minValue + new Random().NextDouble() * (maxValue + minValue), 4);
 
-    }
-    return array;
-}
-void ShowArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+//     }
+//     return array;
+// }
+// void ShowArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
 
-void FindMinMax(double[] array)
-{
-    double minNum = array[0];
-    double maxNum = array[0];
-    // double firstIndex = array[0];
+// void FindMinMax(double[] array)
+// {
+//     double minNum = array[0];
+//     double maxNum = array[0];
+//    
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] >= maxNum) maxNum = array[i];
+//         if (minNum >= array[i]) minNum = array[i];
+//     }
+//     Console.WriteLine();
+//     Console.WriteLine($"The minimal digit of the array is {minNum} and the max digit is {maxNum}.");
+//    
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] >= maxNum) maxNum = array[i];
-        if (minNum >= array[i]) minNum = array[i];
-    }
-    Console.WriteLine();
-    Console.WriteLine($"The minimal digit of the array is {minNum} and the max digit is {maxNum}.");
-    // Console.WriteLine();
-    // Console.WriteLine(minNum);
+// }
 
-
-}
-
-Console.WriteLine("Input array size");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input minimal value of array element");
-double minValue = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input maximal value of array element");
-double maxValue = Convert.ToDouble(Console.ReadLine());
-
-
-
-double[] MyArray = CreatRandomArray(size, minValue, maxValue);
-ShowArray(MyArray);
-FindMinMax(MyArray);
-
-
-// _________________________________________________________________________________________________
-
-// int arrayFindEN = FindEvenNumber(CreatRandomArray(size, 100, 999));
-// ShowArray()
-// ShowArray(CreatRandomArray(size, 100, 999));
-
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Input minimal value of array element");
-// int minValue = Convert.ToInt32(Console.ReadLine());
+// double minValue = Convert.ToDouble(Console.ReadLine());
 // Console.WriteLine("Input maximal value of array element");
-// int maxValue = Convert.ToInt32(Console.ReadLine());
+// double maxValue = Convert.ToDouble(Console.ReadLine());
 
+
+
+// double[] MyArray = CreatRandomArray(size, minValue, maxValue);
+// ShowArray(MyArray);
+// FindMinMax(MyArray);
 
 // _________________________________________________________________________________________________________________________
