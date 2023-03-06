4752958719fe768c452258bfb6a912b9eb686904
Dmitriy Kulakov <dk4git@outlook.com>
2023-03-06 20:46:07 +0300

    work6 is done

--- homeworks/work6/Program.cs
+++ homeworks/work6/Program.cs
@@ -6,45 +6,25 @@
 
 //Решение.
 
-int[] CreatArray(int size) //int minValue, int maxValue)
-{
-    int sumOfIndex = 0;
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine($"Write the not more {size} positive and negative numbers."); // Поместить над for
-        array[i] = Convert.ToInt32(Console.ReadLine());
-        if (array[i] > 0) sumOfIndex += 1;
-    }
-    Console.WriteLine(sumOfIndex);
-    return array;
-}
-
-void ShowArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
-
-// int FindEvenNumber(int[] array)
+// int[] CreatArray(int size)
 // {
-//     int count = 0;
-//     for (int i = 0; i < array.Length; i++)
+//     int sumOfIndex = 0;
+//     int[] array = new int[size];
+//     Console.WriteLine($"Write the not more then {size} positive and negative numbers.");
+//     for (int i = 0; i < size; i++)
 //     {
-//         if (array[i] % 2 == 0) count += 1;
+
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//         if (array[i] > 0) sumOfIndex += 1;
+
 //     }
-//     Console.WriteLine($" The count of even numbers is {count}.");
-//     return count;
+//     Console.WriteLine($"The sum of positive index is {sumOfIndex}.");
+//     return array;
 // }
 
-Console.WriteLine("Input array size");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] MyArray = CreatArray(size);
-ShowArray(CreatArray(size));
-// FindEvenNumber(MyArray);
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] MyArray = CreatArray(size);
 
 //___________________________________________________________________________________________________________________
 
@@ -53,6 +33,33 @@ ShowArray(CreatArray(size));
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x = (b2-b1)/(k1-k2)
+
 //Решение.
+
+double FindCrossPoint(double angCoefk1, double angCoefk2, double coefb1, double coefb2)
+{
+    double x = (coefb2 - coefb1) / (angCoefk1 - angCoefk2);
+    double y = angCoefk1 * x + coefb1;
+    Console.WriteLine($"The intersection point is ({x},{y}).");
+    return y;
+}
+
+Console.WriteLine("Enter an angular coefficient 'b1'. ");
+double coefb1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter an angular coefficient 'k1'. ");
+double angCoefk1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter an angular coefficient 'b2'. ");
+double coefb2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter an angular coefficient 'k2'. ");
+double angCoefk2 = Convert.ToDouble(Console.ReadLine());
+
+
+FindCrossPoint(angCoefk1, angCoefk2, coefb1, coefb2);
+
+
 
 //___________________________________________________________________________________________________________________
